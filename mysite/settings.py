bf4e945670544585934dae6eb31a6f0968a45f18
riddhi <149240684+Riddhi3092002@users.noreply.github.com>
2026-02-22 22:28:12 -0500

    settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tp%s098**+e)(+qdgt&tugfzw9wf9x4+dm%29z1aboiq3_%-o%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangotutorial-env.eba-fpexz6p7.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['djangotutorial-env.eba-fpexz6p7.us-west-2.elasticbeanstalk.com', '*']
 
 
 # Application definition
@@ -116,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
