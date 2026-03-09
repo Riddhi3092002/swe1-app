00b105319b482ed9a3db3bed4a1919c0123f98a3
Ian <43066864+IanC13@users.noreply.github.com>
2026-03-09 18:47:03 -0400

    Add basic test case to polls/tests.py

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,7 @@
-# from django.test import TestCase
-
 # Create your tests here.
+from django.test import TestCase
+
+
+class Test(TestCase):
+    def test_dummy(self):
+        self.assertTrue(False)
